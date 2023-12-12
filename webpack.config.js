8cc1fb6a2107d4a91c19d08ce9fd656f56f47426
sai shravan <saishravan384@gmail.com>
2023-12-12 10:09:59 +0530

    build: Configure webpack to use bundle CSS

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.css$/i,
-      //   use: ['style-loader', 'css-loader'],
-      // },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 }
