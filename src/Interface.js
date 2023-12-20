7051568184806bbb8813c05cb36a7e07ac23b169
sai shravan <saishravan384@gmail.com>
2023-12-20 21:31:21 +0530

    fix: Capitalize main.js filename

--- src/Interface.js
+++ src/Interface.js
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import Main from './components/main'
+import Main from './components/Main'
 import Footer from './components/Footer'
 
 import StartScreen from './components/StartScreen'
